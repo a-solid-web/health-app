eb94b1363e86c443270c0800601d8bf0c8650850
elliott <elliottbrunet@gmail.com>
2019-04-14 00:29:33 +0200

    build

---
+++ precache-manifest.6ebfb8218d98db0859f95574bd2db394.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8ec93641b004dd615db1",
+    "url": "/static/js/main.41d71643.chunk.js"
+  },
+  {
+    "revision": "472f4466ba1580aea5f2",
+    "url": "/static/js/2.56ca21f0.chunk.js"
+  },
+  {
+    "revision": "8ec93641b004dd615db1",
+    "url": "/static/css/main.9f5bd852.chunk.css"
+  },
+  {
+    "revision": "f6cc665a612148da82376b1043c55cc5",
+    "url": "/index.html"
+  }
+];
